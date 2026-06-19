fadeecb37d1b6080fb304f0342f8f48ab9e97e60
vicky-geek <vickygeek435@gmail.com>
2026-06-19 16:18:45 +0530

    added docker in project and also push created image into docker hub

--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS todo_db;
 USE todo_db;
 
 CREATE TABLE IF NOT EXISTS todos (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     task VARCHAR(255) NOT NULL,
     priority INT NOT NULL,
     description TEXT NULL
